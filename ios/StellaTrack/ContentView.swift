50a43027e49607a583dbff6671c0bce3f1fa080d
David To <totinlap@gmail.com>
2026-04-04 02:40:41 +0800

    Initial Commit

---
+++ ios/StellaTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StellaTrack
+//
+//  Created by David To on 4/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/StellaTrack/StellaTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  StellaTrackApp.swift
+//  StellaTrack
+//
+//  Created by David To on 4/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct StellaTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
